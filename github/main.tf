90276b93b4e0bf78fcabe0e0d5e2e797553bdf3b
Samironius <Samirshahubs@gmail.com>
2020-10-27 17:27:10 +0200

    Create a repository using Terraforms

--- github/main.tf
+++ github/main.tf
@@ -1,13 +1,24 @@
 provider "github" {
   token = var.github_token
 }
-
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
+resource "github_repository" "terraform" {
+  name        = "terraform"
+  description = "Create a repository using Terraforms "
+  auto_init   = true
+//  private     = true
+}
+resource "github_branch" "develop" {
+  depends_on = [github_repository.terraform]
+  repository = "terraform"
+  branch     = "develop"
+}
+resource "github_branch_protection" "protection" {
+  repository_id = github_repository.terraform.node_id
+  pattern = "main"
+  enforce_admins = true
 }
 
-data "github_branch" "development" {
-  repository = "example"
-  branch     = "development"
+resource "github_user_ssh_key" "ssh_key" {
+  title = "samirus"
+  key   = file("id_rsa.pub")
 }
